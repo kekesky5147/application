dc141d6c66657f6baa0d2b3fbbe70e9dbb55fefe
Minseok Kim^C <ksj5147@gmail.com>
2024-12-23 11:04:47 -0800

    hidden test

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
 const greeting = document.querySelector('#greeting')
 const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit (event) {
   event.preventDefault()
   const username = loginInput.value
+  localStorage.setItem('USERNAME_KEY', username)
   loginForm.classList.add(HIDDEN_CLASSNAME)
   //   greeting.innerText = 'Hello ' + username
-  greeting.innerText = `Hello ${username}`
+  paintGreetings(username)
+}
 
+function paintGreetings (username) {
+  greeting.innerText = `Hello ${username}`
   greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
+
+const savedUsername = localStorage.getItem('USERNAME_KEY')
+
+console.log(savedUsername)
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+} else {
+  paintGreetings(savedUsername)
+}
