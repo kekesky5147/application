b335499a18f6fe69da571ff2e3007e4d5d2346fc
Minseok Kim^C <ksj5147@gmail.com>
2024-12-26 12:04:01 -0800

    components

--- JS/greeting.js
+++ JS/greeting.js
@@ -6,9 +6,9 @@ const USERNAME_KEY = 'username'
 
 function onLoginSubmit (event) {
   event.preventDefault()
+  loginForm.classList.add(HIDDEN_CLASSNAME)
   const username = loginInput.value
   localStorage.setItem('USERNAME_KEY', username)
-  loginForm.classList.add(HIDDEN_CLASSNAME)
   //   greeting.innerText = 'Hello ' + username
   paintGreetings(username)
 }
@@ -25,6 +25,7 @@ const savedUsername = localStorage.getItem('USERNAME_KEY')
 console.log(savedUsername)
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
 } else {
   paintGreetings(savedUsername)
 }
